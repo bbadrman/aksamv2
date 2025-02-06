54e81cfcacb3374b16f141dfd9251a8d7c1357a7
Badr BECHTIOUI <80051297+bbadrman@users.noreply.github.com>
2025-02-06 10:46:10 +0100

    make style prospect

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -2,13 +2,13 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
+
 use App\Repository\ClientRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
-#[ApiResource()]
+
 class Client
 {
     #[ORM\Id]
--- src/Form/ProspectType.php
+++ src/Form/ProspectType.php
@@ -8,41 +8,225 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\Extension\Core\Type as Type;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 
 class ProspectType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('phone')
-            ->add('email')
-            ->add('gender')
-            ->add('city')
-            ->add('adress')
-            ->add('brithAt', null, [
+            ->add('nom', Type\TextType::class, [
+                'label' => 'Nom    (obligatoir)',
+                'attr' => [
+
+                    'placeholder' => 'Tapez le Nom du Client'
+                ],
+                'required' => true,
+                // 'constraints' => new NotBlank(['message' => 'ne peut pas etre vide'])
+            ])
+            ->add('prenom', Type\TextType::class, [
+                'label' => 'Prénom   (obligatoir) ',
+                'attr' => [
+
+                    'placeholder' => 'Tapez le Prénom du Client'
+                ],
+                'required' => false,
+
+            ])
+            ->add('phone', Type\TelType::class, [
+                'label' => 'Téléphone 1    (obligatoir)',
+                'required' => true,
+                'constraints' => new Length([
+                    'min' => 10,
+                    'minMessage' => '  
+                    le numéro de téléphone doit composer des 10 chiffres y a compris le 0 ',
+                    'max' => 10,
+                    'maxMessage' => '  
+                    le numéro de téléphone doit composer des 10 chiffres y a compris le 0 '
+                ]),
+
+                'attr' => [
+                    'placeholder' => 'Merci de saisir le numéro de téléphone'
+                ]
+            ])
+            ->add('email', Type\EmailType::class, [
+                'label' => 'Email (obligatoir)',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => "Merci de saisir l'adresse email"
+                ]
+            ])
+            ->add('gender', Type\ChoiceType::class, [
+                'label' => 'Genre',
+                'required' => false,
+                'placeholder' => '--Merci de Choisir le genre--',
+                'choices' => [
+                    'Male' => 1,
+                    'Female' => 2
+                ],
+                'expanded' => false,
+                'multiple' => false
+            ])
+            ->add('city', Type\TextType::class, [
+                'label' => 'Ville ',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Ville du client',
+                ]
+            ])
+            ->add('adress', Type\TextareaType::class, [
+                'label' => 'Addresse complét (obligatoir)',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Address compltét du client',
+                ]
+            ])
+            ->add('brithAt', BirthdayType::class, [
+                'label' => 'Date de Naissance ',
                 'widget' => 'single_text',
+                'required' => false,
             ])
-            ->add('source')
-            ->add('typeProspect')
-            ->add('raisonSociale')
-            ->add('codePost')
-            ->add('gsm')
-            ->add('assurer')
-            ->add('lastAssure')
-            ->add('motifResil')
-            ->add('motifSaise')
-            ->add('url')
-            ->add('secdEmail')
-            ->add('creatAt', null, [
-                'widget' => 'single_text',
+            // ->add('source', Type\TextType::class, [
+            //     'label' => 'Source',
+            //     'required' => true,
+            //     'data' => '1',  
+
+            //     'attr' => ['readonly' => true],  
+            // ])
+            ->add('typeProspect', Type\ChoiceType::class, [
+                'label' => 'Type Pospect ',
+                'required' => true,
+                'placeholder' => '--Merci de selectie-- ',
+                'choices' => [
+                    'Particulier' =>  '1',
+                    'Professionnel' => '2',
+                ],
+                'expanded' => false,
+                'multiple' => false,
+
             ])
-            ->add('activites')
-            ->add('comrcl', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('raisonSociale', TextType::class, [
+                'label' => 'Raison sociale ',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Raison sociale',
+                ]
             ])
+            ->add('codePost', TextType::class, [
+                'label' => 'Code Postal (obligatoir)',
+                'constraints' => new Length(['min' => 5,  'minMessage' => 'le code postale doit etre quatre caactaire mini', 'max' => 5, 'maxMessage' => 'le code postale doite etre 5 caractaire max']),
+                'attr' => [
+                    'placeholder' => 'Merci de saisir le Code Postal',
+                ]
+            ])
+            ->add('gsm', Type\TelType::class, [
+                'label' => 'Téléphone 2',
+                'required' => false,
+                'constraints' => new Length([
+                    'min' => 10,
+                    'minMessage' => '  
+                    le numéro de téléphone doit composer des 10 chiffres y a compris le 0 ',
+                    'max' => 10,
+                    'maxMessage' => '  
+                    le numéro de téléphone doit composer des 10 chiffres y a compris le 0 '
+                ]),
+                'attr' => [
+                    'placeholder' => 'Merci de saisir si la deuxieme numéro de téléphone'
+                ]
+            ])
+            ->add('assurer', Type\ChoiceType::class, [
+                'label' => 'Assuré actuellement',
+                'required' => false,
+                'placeholder' => '--Merci de selectie-- ',
+                'choices' => [
+                    'Oui' => 'Oui',
+                    'Non' => 'Non'
+                ],
+                'expanded' => false,
+                'multiple' => false
+            ])
+            ->add('lastAssure', Type\ChoiceType::class, [
+                'label' => 'Ancienne assurance résilié ',
+                'required' => false,
+                'placeholder' => '--Merci de selectie-- ',
+                'choices' => [
+                    'Oui' => 'Oui',
+                    'Non' => 'Non'
+                ],
+                'expanded' => false,
+                'multiple' => false
+            ])
+            ->add(
+                'motifResil',
+                Type\ChoiceType::class,
+                [
+                    'label' => 'Motif résiliation ',
+                    'required' => false,
+                    'placeholder' => '--Merci de selectie-- ',
+                    'choices' => [
+                        'Aggravation de risque' =>  1,
+                        'Amiable' =>  2,
+                        'Échéance' => 3,
+                        'Non-paiement' => 4,
+                        'Sinistre' =>  5,
+                        'Suspension de permis' => 6,
+                        'Fausse déclaration' => 7
+                    ],
+                    'expanded' => false,
+                    'multiple' => false
+                ]
+            )
+            ->add('motifSaise', Type\ChoiceType::class, [
+                'label' => 'Motive de saisir ',
+                'required' => true,
+                'placeholder' => '--Merci de selectie-- ',
+                'choices' => [
+                    'Parrainage' => '1',
+                    'Appel Entrant' => '2',
+                    'Avenant' => '3',
+                    'Ancienne contrat' => '4',
+                    'Propre site' => '5',
+                    'Revendeur' => '6',
+                    'Test' => '7',
+                ],
+                'expanded' => false,
+                'multiple' => false
+            ])
+            // ->add('url')
+            // ->add('secdEmail')
+            // ->add('creatAt', null, [
+            //     'widget' => 'single_text',
+            // ])
+            ->add(
+                'activites',
+                Type\ChoiceType::class,
+                [
+                    'label' => 'Activites ',
+                    'required' => false,
+                    'placeholder' => '--Merci de selectie-- ',
+                    'choices' => [
+                        'TPM' =>  1,
+                        'VTC' =>  2,
+                        'Sociéte' => 3,
+                        'Décenale' => 4,
+                        'Dommage' =>  5,
+                        'Marchandise' =>  6,
+                        'Négociant' =>  7,
+                        'Prof auto' =>  8,
+                        'Garage' => 9
+                    ],
+                    'expanded' => false,
+                    'multiple' => false,
+                    // 'constraints' => [
+                    //     new Callback([$this, 'validateActivites'])
+                    // ]
+                ]
+            )
+            // ->add('products')
+            ->add('comrcl')
         ;
     }
 
